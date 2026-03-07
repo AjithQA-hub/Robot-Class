bb74819a61c0b82e32a4810d5eb43a963a3ef619
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-03-07 10:01:31 +0530

    Update RobotTask3.java

--- InmakesRobot/src/org/robot/RobotTask3.java
+++ InmakesRobot/src/org/robot/RobotTask3.java
@@ -3,7 +3,6 @@ package org.rob;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -56,3 +55,4 @@ public class RobotTask3 {
 			driver.close();
 
 }}
+
